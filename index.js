623c91232def060d5acf006ca6dc88cd5a257b0d
Riley <r.pannkuk@gmail.com>
2018-01-10 09:09:04 -0800

    Always online for Heroku

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
+const express = require('express');
+const app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+const port = process.env.PORT || 5000;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the `public` directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', (request, response) => {
+    // ejs render automatically looks in the views folder
+    response.render('index');
+});
+
+app.listen(port, () => {
+    // will echo 'Our app is running on http://localhost:5000 when run locally'
+    console.log('Our app is running on http://localhost:' + port);
+});
+
+
+setInterval(() => {
+    http.get('http://your-app-name.herokuapp.com');
+  }, 900000);
+
+
+/******************* */
+
+
 require('dotenv').config();
 
 const Discord = require('discord.js');
