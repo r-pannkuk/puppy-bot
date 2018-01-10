65db0b5b56331605d129e7cf1240f7bd40d4a1d3
Riley <r.pannkuk@gmail.com>
2018-01-10 02:58:02 -0800

    Updated configuration file for Heroku Readiness.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 const Discord = require('discord.js');
 const commando = require('discord.js-commando');
 const bot = new commando.Client({
-    owner: '260288776360820736',
+    owner: process.env.USER,
     unknownCommandResponse: false
 });
 
@@ -9,4 +11,4 @@ bot.registry.registerGroup('rng', 'Random');
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + '/commands');
 
-bot.login('MzMxNjY5MDg2ODEzODE0Nzg0.DTczvA.sI8aNlARrl6VGDYdPkWJeCpLyfE');
+bot.login(process.env.TOKEN);
