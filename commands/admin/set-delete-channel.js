9e00410b5d086f25bf989bdc223bf9dd1f81d3a1
Riley <r.pannkuk@gmail.com>
2018-08-01 20:53:25 -0700

    Deletion Channel (for real this time)

---
+++ commands/admin/set-delete-channel.js
@@ -0,0 +1,47 @@
+const commando = require('discord.js-commando');
+const Discord = require('discord.js');
+
+
+module.exports = class SetDeleteChannel extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'set-delete-channel',
+            group: 'admin',
+            memberName: 'set-delete-channel',
+            description: 'Stores a text channel for reporting deleted messages.',
+            examples: [ '!set-delete-channel #channel' ],
+            argsPromptLimit: 0,
+            args: [
+                {
+                    key: 'channel',
+                    prompt: "Store a channel for use.",
+                    type: 'channel'
+                }
+            ]
+        });
+    }
+
+    
+    async run(message, { channel }) {
+        var client = this.client;
+
+        client.on('messageDelete', (message) => {
+            if(message.author !== client.user) {
+
+                if(channel !== null) {
+                    var embed = new Discord.RichEmbed();
+                    embed.setAuthor(message.author.username, message.author.displayAvatarURL);
+                    embed.setTitle(message.channel.name)
+                    embed.setTimestamp(message.timestamp);
+                    embed.setDescription(message.content);
+                    channel.send(embed);
+                }
+                else {
+                    console.log('channel not found');
+                }
+            }
+        });
+
+        message.channel.send(`Deletion channel set to ${channel}.`);
+    }
+}
---
+++ messageListeners/checkForTraps.js
@@ -0,0 +1,23 @@
+module.exports = function(message, client) {
+    if(message.channel.type !== 'text') {
+        return;
+    }
+
+    var content = message.content.toLowerCase();
+    var traps = client.battleSystem._enmap.get('traps');
+
+    var validKey = Object.keys(traps).find(key => 
+        content.indexOf(key) > -1 && traps[key].messageId !== message.id
+    );
+
+    if(validKey !== undefined) {
+
+        if(content === `!disarmtrap ${validKey}` || content === `!removetrap ${validKey}`) {
+            return;
+        }
+
+        client.emit('sprungTrap', { message: message, key: validKey });
+
+        client.battleSystem.springTrap(message, validKey);
+    }
+};
