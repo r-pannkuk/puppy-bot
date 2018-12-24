24b4aae34901adbeb3bf251781d057ea8f894bd2
Riley <r.pannkuk@gmail.com>
2018-12-24 03:00:22 -0800

    No more free bread

--- commands/games/mudamaid6/free-bread.js
+++ commands/games/mudamaid6/free-bread.js
@@ -14,43 +14,12 @@ module.exports = class FreeBreadCommand extends commando.Command {
             group: 'games',
             memberName: 'free-bread',
             description: 'Free rolls for everyone!',
-            examples: [ '!free-bread' ],
-            args: [
-                {
-                    key: 'type',
-                    prompt: 'What type of bread to have.',
-                    type: 'string',
-                    default: ''
-                }
-            ]
+            examples: [ '!free-bread' ]
         });
     }
 
     
     async run(message, {type}) {
-        type = type.toLowerCase();
-        
-        if(type === '') {
-            type = 'm';
-        }
-
-        if(type === 'husbando' || type === 'h' ||
-            type === 'waifu' || type === 'w' ||
-            type === 'husbandog' || type === 'hg' ||
-            type === 'waifug' || type === 'wg' ||
-            type === 'marry' || type === 'm' ||
-            type === 'marryg' || type === 'mg') {
-            message.channel.send(`$${type}`);
-        }
-        else {
-            message.channel.send(`Invalid format. Please omit or use one of the following:
--- husbando (h)
--- waifu (w)
--- marry (m)
--- husbandog (hg)
--- waifug (wg)
--- marryg (mg)`);
-        }
-
+        message.channel.send(`:roll:`);
     }
 }
