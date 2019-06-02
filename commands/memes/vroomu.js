9d999aec88390d7c714e84905ebb8736d7154d4d
Riley Pannkuk <r.pannkuk@gmail.com>
2019-06-01 23:05:23 -0700

    Vroomu

---
+++ commands/memes/vroomu.js
@@ -0,0 +1,21 @@
+const commando = require('discord.js-commando');
+const pyShell = require('python-shell');
+
+
+module.exports = class VroomuCommands extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'vroomu',
+            group: 'memes',
+            memberName: 'vroomu',
+            description: 'This is really dumb.',
+            examples: ['!vroomu']
+        });
+    }
+
+    async run(message) {
+        message.channel.send({
+            files: ['./commands/memes/templates/vroomu.png']
+        });
+    }
+}
