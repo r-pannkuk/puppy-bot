7f3625591fa21aee786e094f34f0da6495879585
Riley Pannkuk <r.pannkuk@gmail.com>
2020-06-03 03:26:30 -0700

    Magneto memes

---
+++ commands/memes/magneto.js
@@ -0,0 +1,61 @@
+const commando = require('discord.js-commando');
+const pyShell = require('python-shell');
+
+
+module.exports = class MagnetoCommand extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'magneto',
+            group: 'memes',
+            memberName: 'magneto',
+            description: 'This is really dumb.',
+            examples: ['!magneto http://bit.ly/2DaER2o'],
+            argsPromptLimit: 0,
+            args: [
+                {
+                    key: 'source',
+                    prompt: "Please enter a duwang source.",
+                    type: 'string',
+                    infinte: true,
+                    default: ''
+                }
+            ]
+        });
+    }
+
+    async run(message, { source }) {
+        // Detect if image exists and override with image if so
+        if (message.attachments.size > 0) {
+            var attachments = message.attachments.entries();
+            for (const [key, value] of attachments) {
+                var url = value.url;
+                //True if this url is a png image.
+                if(url.match(/\.(jpeg|jpg|gif|png)$/) != null) {
+                    source = url;
+
+                    pyShell.run('duwang.py', {
+                        mode: 'text',
+                        pythonOptions: ['-u'],
+                        pythonPath: 'python3',
+                        scriptPath: './commands/memes/scripts/',
+                        args: [source]
+                    }, function (err, results) {
+                        console.log(err);
+                        // Trim white space and carriage return from the call
+                        if (results === undefined) {
+                            results = [];
+                        }
+                        results = results.map((value) => value.replace(/\s+/g, ''));
+                        message.channel.send({
+                            files: results
+                        });
+                    });
+
+                    return;
+                }
+            }
+        }
+
+        message.channel.send("Image was not found.  Please enter a valid image.")
+    }
+}
