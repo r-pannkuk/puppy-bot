7596a6929fd5045bc0cc7e267f13c8698c14b441
Riley Pannkuk <Riley.Pannkuk@doubledown.com>
2021-06-22 03:03:08 -0700

    Choices for superturn / omedetou.

--- commands/memes/congratulations.js
+++ commands/memes/congratulations.js
@@ -1,27 +1,70 @@
 const commando = require('discord.js-commando');
 const pyShell = require('python-shell');
 const fs = require('fs');
+const path = require('path');
+const { toLowerCase } = require('ffmpeg-static');
 
 
 module.exports = class CongratulationsCommand extends commando.Command {
     constructor(client) {
+        var directory = './commands/memes/media/congratulations/';
+
+        var files = fs.readdirSync(directory);
+        var options = {}
+
+        var description = `This is really dumb. Options: `
+
+        for(var i in files) {
+            var name = path.parse(files[i]).name.toLowerCase()
+            options[name] = directory + files[i];
+            description += `${name}, `
+        }
+
+        description = description.slice(0, description.length - 2)
+
         super(client, {
             name: 'congratulations',
             group: 'memes',
             aliases: ['congraturations', 'omedetou'],
             memberName: 'congratulations',
-            description: 'This is really dumb.',
-            examples: ['!congratulations']
+            description: description,
+            examples: ['!congratulations', `!congratulations ${Object.keys(options)[0]}`],
+            args: [
+                {
+                    key: 'choice',
+                    prompt: "Select a specific image to use from the list.",
+                    type: 'string',
+                    infinte: true,
+                    default: ''
+                }
+            ]
         });
+
+        this.directory = directory;
+        this.options = options;
     }
 
-    async run(message) {
-        var directory = './commands/memes/media/congratulations/';
+    /**
+     * 
+     * @param {Object} message - The message invoking this command. 
+     * @param {Object} object
+     * @param {string} choice - The choice to use in the image. 
+     */
+    async run(message, { choice }) {
+        var keys = Object.keys(this.options)
+        if(choice === '') {
+            choice = keys[Math.floor(Math.random() * keys.length)];
+        } else {
+            choice = choice.toLowerCase();
 
-        var files = fs.readdirSync(directory);
+            if(keys.indexOf(choice) === -1) {
+                message.channel.send(`Could not find the file specified.  Please choose from the list: ${keys.join(', ')}`)
+                return;
+            }
+        }
 
         message.channel.send({
-            files: [directory + files[Math.floor(Math.random() * files.length)]]
+            files: [this.options[choice]]
         });
     }
 }
--- commands/memes/superturn.js
+++ commands/memes/superturn.js
@@ -1,26 +1,62 @@
 const commando = require('discord.js-commando');
 const pyShell = require('python-shell');
 const fs = require('fs');
+const path = require('path');
 
 
 module.exports = class SuperturnCommands extends commando.Command {
     constructor(client) {
+        var directory = './commands/memes/media/superturn/';
+
+        var files = fs.readdirSync(directory);
+        var options = {}
+
+        var description = `This is really dumb. Options: `
+
+        for(var i in files) {
+            var name = path.parse(files[i]).name
+            options[name] = directory + files[i];
+            description += `${name}, `
+        }
+
+        description = description.slice(0, description.length - 2)
+
         super(client, {
             name: 'superturn',
             group: 'memes',
             memberName: 'superturn',
-            description: 'This is really dumb.',
-            examples: ['!superturn']
+            description: description,
+            examples: ['!superturn', `!superturn ${Object.keys(options)[0]}`],
+            args: [
+                {
+                    key: 'choice',
+                    prompt: "Select a specific image to use from the list.",
+                    type: 'string',
+                    infinte: true,
+                    default: ''
+                }
+            ]
         });
+
+        this.directory = directory;
+        this.options = options;
     }
 
-    async run(message) {
-        var directory = './commands/memes/media/superturn/';
+    async run(message, { choice }) {
+        var keys = Object.keys(this.options)
+        if(choice === '') {
+            choice = keys[Math.floor(Math.random() * keys.length)];
+        } else {
+            choice = choice.toLowerCase();
 
-        var files = fs.readdirSync(directory);
+            if(keys.indexOf(choice) === -1) {
+                message.channel.send(`Could not find the file specified.  Please choose from the list: ${keys.join(', ')}`)
+                return;
+            }
+        }
 
         message.channel.send({
-            files: [directory + files[Math.floor(Math.random() * files.length)]]
+            files: [this.options[choice]]
         });
     }
 }
