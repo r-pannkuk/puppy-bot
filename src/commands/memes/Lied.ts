6bc4f6ed33fb6e4a13b22a9afa21a727691a3f9e
Riley Pannkuk <Klypyon@gmail.com>
2024-08-08 18:44:39 -0700

    Liar.

---
+++ src/commands/memes/Lied.ts
@@ -0,0 +1,75 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import type { ApplicationCommandRegistry, Args, ChatInputCommandContext, ContextMenuCommandContext } from '@sapphire/framework';
+import type { ChatInputCommandInteraction, ContextMenuCommandInteraction, Message } from 'discord.js';
+import { PyScriptCommand } from '../../lib/structures/command/PyScriptCommand';
+
+const SHORT_DESCRIPTION = 'Tell the truth, the whole truth, nothing but the truth.'
+
+@ApplyOptions<PyScriptCommand.Options>({
+    name: 'lied',
+    description: SHORT_DESCRIPTION,
+    detailedDescription: SHORT_DESCRIPTION + ' Examples:\n' +
+        '-- /lied --user=@user --text=text\n',
+    requiredUserPermissions: ["SendMessages"],
+    requiredClientPermissions: ["SendMessages"],
+    scriptName: 'lied.py',
+    nsfw: false,
+    options: ['user', 'text']
+})
+export class LiedCommand extends PyScriptCommand {
+    public override registerApplicationCommands(registry: ApplicationCommandRegistry) {
+        registry.registerChatInputCommand((builder) => builder
+            .setName(this.name)
+            .setDescription(this.description)
+            .addUserOption((option) =>
+                option
+                    .setName("user")
+                    .setDescription("Target a user for telling the truth.")
+                    .setRequired(true)
+            )
+            .addStringOption((option) =>
+                option
+                    .setName("text")
+                    .setDescription("Enter what you want the user to say.")
+                    .setRequired(true)
+            )
+            ,
+            this.slashCommandOptions
+        )
+
+        registry.registerContextMenuCommand((builder) => builder
+            .setName('Meme - Liar')
+            .setType(3 /* ContextMenuCommandType.MESSAGE */)
+            ,
+            this.contextCommandOptions
+        )
+    }
+
+    public override async chatInputRun(interaction: ChatInputCommandInteraction, _context: ChatInputCommandContext) {
+        const user = interaction.options.getUser('user', true)
+        const member = interaction.guild?.members.cache.get(interaction.options.getUser('user', true)?.id);
+        const text = interaction.options.getString('text', true);
+        await interaction.deferReply()
+        const files = await this.run([member!.displayAvatarURL() || user.displayAvatarURL() || member!.avatar || user.avatar || "", member!.displayName! || user.username, text!]);
+        await interaction.editReply({ files: files });
+    }
+
+    public override async contextMenuRun(interaction: ContextMenuCommandInteraction, _context: ContextMenuCommandContext) {
+        if (interaction.isMessageContextMenuCommand()) {
+            const message = interaction.targetMessage;
+            const text = message.content;
+            await interaction.deferReply();
+            const files = await this.run([message.author.displayAvatarURL() || "", message.author.displayName, text!]);
+            await interaction.editReply({ files: files });
+        } else {
+            await interaction.deleteReply();
+        }
+    }
+
+    public override async messageRun(message: Message, args: Args) {
+        const member = message.guild?.members.cache.get(args.getOption('user')!);
+        const text = args.getOption('text');
+        const files = await this.run([member!.displayAvatarURL() || member!.avatar || "", member!.displayName!, text!]);
+        message.channel.send({ files: files });
+    }
+}
--- src/scripts/lied.py
+++ src/scripts/lied.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw
 import hashlib
 from pathlib import Path
 from urllib.parse import urlparse
@@ -9,9 +9,12 @@ import requests
 from io import BytesIO
 import sys
 import os
+import textwrap
+
 
 MAX_THRESHOLD = 30
 MIN_THRESHOLD = 10
+MAX_TEXT_BLOCK_LENGTH = 130
 
 # Args:
 # 1. URL of the avatar
@@ -19,17 +22,17 @@ MIN_THRESHOLD = 10
 # 3. Text to display
 
 def main():
-    print(sys.argv)
-
     # Determine if image exists in save files
-    location = sys.argv[1]
+    avatar_url = sys.argv[1]
+    username = sys.argv[2]
+    text = sys.argv[3]
     save_folder = "./saves/lied/"
-    save_name = save_folder + hashlib.sha1(location.encode()).hexdigest() + ".png"
+    save_name = save_folder + hashlib.sha1(avatar_url.encode()).hexdigest() + '_' + hashlib.sha1(username.encode()).hexdigest() + '_' + hashlib.sha1(text.encode()).hexdigest() + ".png"
 
     # check for already created images
-    # if(os.path.isfile(save_name)):
-    #     print(save_name)
-    #     return
+    if(os.path.isfile(save_name)):
+        print(save_name)
+        return
     
     if(not os.path.isdir(save_folder)):
         Path(save_folder).mkdir(parents=True, exist_ok=True)
@@ -61,13 +64,16 @@ def main():
     thumbnail_size = (260, 260)
 
     # determine if this is a URL
-    if(urlparse(location).scheme != ""):
-        response = requests.get(location)
+    if(urlparse(avatar_url).scheme != ""):
+        response = requests.get(avatar_url)
 
         user_img = Image.open(BytesIO(response.content)).convert("RGBA")
 
         # scale image to proper dimensions for a picture
         user_img.thumbnail(thumbnail_size)
+
+        if user_img.width < thumbnail_size[0]:
+            user_img = user_img.resize(thumbnail_size)
 
         paste_loc = (
             (int)(pos1[0] + (thumbnail_size[0] - user_img.width) / 2), 
@@ -79,6 +85,89 @@ def main():
 
     # Paste the overlay of the hand on top of the image
     new_img.paste(overlay, (0,0), overlay)
+
+    # Draw the username #
+    MAX_LINE = 'FFFFFFFF'
+    font = ImageFont.truetype("./src/assets/fonts/animeace2_ital.ttf", 30)
+    font_height = (font.getbbox('T')[1] - font.getbbox('T')[3]) * 1.25
+    max_width = font.getbbox(MAX_LINE)[2] - font.getbbox(MAX_LINE)[0]
+
+    text_start_offset = [
+        53,
+        395 + len(textwrap.wrap(username, width=len(MAX_LINE))) * font_height
+    ]
+
+    for line in textwrap.wrap(username, width=len(MAX_LINE)):
+        line_box = font.getbbox(line)
+        ImageDraw.Draw(new_img).text(
+            (
+                text_start_offset[0] - (line_box[2] - line_box[0]) / 2 + max_width / 2,
+                text_start_offset[1]
+            ), 
+            line, 
+            fill=(35,35,35,255), 
+            font=font
+        )
+        text_start_offset[1] -= font_height
+
+    # Draw the text #
+    MAX_LINE = 'WWWWWWWW'
+
+    font = ImageFont.truetype("./src/assets/fonts/animeace2_ital.ttf", 23)
+    font_height = (font.getbbox('T')[1] - font.getbbox('T')[3]) * 1.5
+    max_width = font.getbbox(MAX_LINE)[2] - font.getbbox(MAX_LINE)[0]
+
+    if len(text) <= MAX_TEXT_BLOCK_LENGTH:
+        text_block_1 = text
+        text_block_2 = None
+    else:
+        split_index = text.rfind(' ', 0, MAX_TEXT_BLOCK_LENGTH)
+        if split_index == -1:
+            split_index = MAX_TEXT_BLOCK_LENGTH
+        text_block_1 = text[:split_index].rstrip()
+        text_block_2 = text[split_index:].lstrip()
+
+      # Text Block 1 #
+
+    text_start_offset = [
+        490, 
+        200 + len(textwrap.wrap(text_block_1, width=int(len(MAX_LINE) * 1.4))) * font_height / 2
+    ]
+
+    for line in textwrap.wrap(text_block_1, width=int(len(MAX_LINE) * 1.4)):
+        line_box = font.getbbox(line)
+        ImageDraw.Draw(new_img).text(
+            (
+                text_start_offset[0] - (line_box[2] - line_box[0]) / 2 + max_width / 2,
+                text_start_offset[1]
+            ), 
+            line, 
+            fill=(0,0,0,255), 
+            font=font
+        )
+        text_start_offset[1] -= font_height
+    
+    # Text Block 2 #
+    MAX_LINE = 'WWWWWWW'
+
+    if text_block_2 is not None:
+        text_start_offset = [
+            310, 
+            375 + len(textwrap.wrap(text_block_2, width=int(len(MAX_LINE) * 1.5))) * font_height / 2
+        ]
+
+        for line in textwrap.wrap(text_block_2, width=int(len(MAX_LINE) * 1.5)):
+            line_box = font.getbbox(line)
+            ImageDraw.Draw(new_img).text(
+                (
+                    text_start_offset[0] - (line_box[2] - line_box[0]) / 2 + max_width / 2,
+                    text_start_offset[1]
+                ), 
+                line, 
+                fill=(0,0,0,255), 
+                font=font
+            )
+            text_start_offset[1] -= font_height
 
     # save
     new_img.save(save_name)
